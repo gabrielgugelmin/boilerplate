b1139644f9da52a4b4338dc7221858af3ae74dd4
gabriel favilla gugelmin <gb.fg@hotmail.com>
2019-02-27 21:37:22 -0300

    Menu, banner e accordion no mobile

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -23,8 +23,8 @@ $(function () {
     }
   });
 
-  // slider de depoimentos
-  $('.js-depo-slider').slick({
+  // slider baner
+  $('.js-banner-slider').slick({
     arrows: true,
     prevArrow: '<button type="button" class="depo__arrow depo__arrow--prev"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 54.5 95" style="enable-background:new 0 0 54.5 95;" xml:space="preserve"><g><path fill="#145f93" d="M8.7,1.5C7.7,0.5,6.5,0,5.1,0S2.5,0.6,1.5,1.5C0.5,2.5,0,3.7,0,5.1s0.5,2.6,1.5,3.6l38.9,38.8L1.5,86.3 c-1,1-1.5,2.2-1.5,3.6s0.5,2.6,1.5,3.6S3.7,95,5,95c1.2,0,2.5-0.5,3.5-1.4l46-46L8.7,1.5z"/></g></svg></button>',
     nextArrow: '<button type="button" class="depo__arrow depo__arrow--next"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 54.5 95" style="enable-background:new 0 0 54.5 95;" xml:space="preserve"><g><path fill="#145f93" d="M8.7,1.5C7.7,0.5,6.5,0,5.1,0S2.5,0.6,1.5,1.5C0.5,2.5,0,3.7,0,5.1s0.5,2.6,1.5,3.6l38.9,38.8L1.5,86.3 c-1,1-1.5,2.2-1.5,3.6s0.5,2.6,1.5,3.6S3.7,95,5,95c1.2,0,2.5-0.5,3.5-1.4l46-46L8.7,1.5z"/></g></svg></button>',
@@ -38,6 +38,27 @@ $(function () {
   // SMOOTH SCROLL
   $('.js-scroll').on('click', function(event) {
     smoothScroll();
+  });
+
+  $('.accordion > li:eq(0) a').addClass('active').next().slideDown();
+
+  $('.accordion__trigger').click(function(e) {
+    console.log($(this));
+    var dropDown = $(this).siblings('.accordion__content');
+    console.log(dropDown);
+
+    $(this).closest('.accordion').find('.accordion__content').not(dropDown).slideUp();
+
+    if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+    } else {
+        $(this).closest('.accordion').find('a.active').removeClass('active');
+        $(this).addClass('active');
+    }
+
+    dropDown.stop(false, true).slideToggle();
+
+    e.preventDefault();
   });
 });
 
